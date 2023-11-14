5cb4a7bd1e8efa7bfdca25d74008a7320f3f5247
schikimikiviki <viktoria.bzdyk@gmx.at>
2023-11-14 19:31:21 +0100

    initial commit

---
+++ backend/src/main/java/running_activity_tracker/tracker/TrackerApplication.java
@@ -0,0 +1,13 @@
+package running_activity_tracker.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/running_activity_tracker/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package running_activity_tracker.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
