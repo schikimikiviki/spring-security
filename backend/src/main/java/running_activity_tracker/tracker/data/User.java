c86a761e74ed25784a1be32776ec7b8ef71d07e5
schikimikiviki <viktoria.bzdyk@gmx.at>
2023-11-18 10:02:38 +0100

    user class

---
+++ backend/src/main/java/running_activity_tracker/tracker/data/User.java
@@ -0,0 +1,51 @@
+package running_activity_tracker.tracker.data;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String password;
+
+    @ElementCollection
+    private Set<String> authorities;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<String> authorities) {
+        this.authorities = authorities;
+    }
+}
